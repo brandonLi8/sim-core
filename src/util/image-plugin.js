e0f3c17a6047f137e0b4415e10af43e7f4d4ee2f
Brandon Li <brandon.li820@icloud.com>
2019-11-13 18:18:22 -0700

    #9 first pass at the image plugin

--- src/util/image-plugin.js
+++ src/util/image-plugin.js
@@ -4,12 +4,13 @@
  * A sim-specific Image plugin for requirejs that allows for the dynamic loading of images in the loader.
  *
  * ## Usage
+ *
  *   1. In your requirejs config, add this file as a path.
  *   ```
  *    requirejs.config( {
  *      paths: {
  *
- *        image: '../node_modules/sim-core/src/image',
+ *        image: '../node_modules/sim-core/src/util/image-plugin',
  *        ...
  *      }
  *    } );
@@ -18,9 +19,64 @@
  *
  *   If the simulation is started normally in `Sim.js`, all images will be loaded in `Loader.js`.
  *
+ *   IMPORTANT: This plugin assumes that you use your namespace path to the `src` or `js` directory to reference images.
+ *              In addition, your images should be located within the `image` directory (sub paths are ok).
+ *
  * ## Implementation
+ *
  *  - For the implementation of the loader, this plugin registers images into the global window object.
  *    This is referenced in `Loader.js` and dynamically loaded, allowing for the loader to load all images.
  *
+ *  - For more background on requirejs plugins, see https://requirejs.org/docs/plugins.html.
+ *
  * @author Brandon Li <brandon.li820@gmail.com>
  */
+
+define( require => {
+  'use strict';
+
+  // modules
+  const DOMObject = require( 'SIM_CORE/display/DOMObject' );
+
+
+  return {
+
+    /**
+     * Function that is called to load a image DOMObject when the image plugin is used.
+     * For more documentation: see https://requirejs.org/docs/plugins.html#api.
+     * @public
+     *
+     * @param {string} name - the name of the image resource to load. For instance, 'image!bar' would the name 'bar'.
+     * @param {function} parentRequire - local require function to load other modules. Contains many utilities.
+     * @param {function} onload - function to call with the return value of the load.
+     * @param {object} config - the requirejs configuration object
+     */
+    load: ( name, parentRequire, onload, config ) => {
+
+      // This plugin assumes that you use your path to the `src` or `js` directory to reference the image.
+      // In addition, your images should be located within the `image` directory (sub paths are ok).
+      // As a result, we reference the the image path after the namespace.
+      // For example: 'SIM_CORE/types/button.jpg' turns into `types/button.jpg`;
+      const imagePath = name.substring( name.indexOf( '/' ) );
+
+      // Reference the name as the true image src, adding the image directory and path.
+      const imageSrc = parentRequire.toUrl( name.substring( 0, name.indexOf( '/' ) ) ) + '/../images' + imagePath;
+
+      //----------------------------------------------------------------------------------------
+      // Create the DOMObject to hold the image.
+      const image = new DOMObject( {
+        type: 'img'
+      } );
+
+      // If the image src fails to load, throw an error.
+      image.element.onerror = error => {
+        throw new Error( `invalid image src: ${ image.element.src }` );
+      };
+
+      // Set the src of the image.
+      image.src = imageSrc;
+
+      onload( image );
+    }
+  };
+} );
