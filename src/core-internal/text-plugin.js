72396ceacaf3a98e4d841bee9d9c41dcad556c1c
Brandon Li <brandon.li820@icloud.com>
2019-12-01 00:21:53 -0700

    #14 create a get request to a functional text plugin

--- src/core-internal/text-plugin.js
+++ src/core-internal/text-plugin.js
@@ -31,4 +31,44 @@
 define( require => {
   'use strict';
 
+  return {
+
+    /**
+     * Function that is called to load the raw text when the text plugin is used.
+     * For more documentation: see https://requirejs.org/docs/plugins.html#api.
+     * @public
+     *
+     * @param {string} name - the name of the image resource to load. For example, 'text!bar' would have the name 'bar'
+     * @param {function} parentRequire - local require function to load other modules. Contains many utilities.
+     * @param {function} onload - function to call with the return value of the load.
+     * @param {object} config - the requirejs configuration object
+     */
+    load( name, parentRequire, onload, config ) {
+
+      // Reference the name as the true path to the raw text file.
+      const textPath = parentRequire.toUrl( name )
+
+      // Create an HTTP Request via XMLHttpRequest for the given textPath.
+      const HTTPRequest = new XMLHttpRequest();
+
+      // Create a GET request to the textPath to access the raw text.
+      // See https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/GET for background.
+      // See https://stackoverflow.com/questions/247483/http-get-request-in-javascript for implementation background.
+      HTTPRequest.open( 'GET', textPath, true ); // true for asynchronous request.
+
+      // Listen to when the asynchronous HTTP request status changes.
+      // See https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/onreadystatechange for context.
+      HTTPRequest.onreadystatechange = () => {
+
+        // Use status codes to check if the request was successful or not.
+        // For full documentation on all status codes, see https://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
+
+        // If the request was successful, load the raw text.
+        if ( HTTPRequest.readyState === 4 && HTTPRequest.status === 200 ) {
+          onload( HTTPRequest.responseText );
+        }
+      };
+      HTTPRequest.send( null );
+    }
+  };
 } );
