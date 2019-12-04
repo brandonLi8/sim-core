53b63cbf50221fc6947bc30684379d2db2772ccc
Brandon Li <brandon.li820@icloud.com>
2019-12-04 12:36:31 -0700

    #19 add properties

--- src/util/ModelViewTransform.js
+++ src/util/ModelViewTransform.js
@@ -1,1 +1,56 @@
 // Copyright © 2019 Brandon Li. All rights reserved.
+
+/**
+ * Transform between model and view coordinate frames, with convenience methods for conversions.
+ *
+ * Requires that the transform is "aligned", i.e., it can be built only from component-wise translation and scaling.
+ * Equivalently, the output x coordinate should not depend on the input y, and the output y shouldn't depend on the
+ * input x.
+ *
+ * The convention is that the y axis is inverted for the view and that the view origin as at the top-left of the
+ * ScreenView. Inverting the y axis is commonly necessary since +y is usually up in textbooks and -y is down in
+ * pixel coordinates. This transform assumes that this is the case.
+ *
+ * @author Brandon Li <brandon.li820@gmail.com>
+ */
+
+define( require => {
+  'use strict';
+
+  // modules
+  const assert = require( 'SIM_CORE/util/assert' );
+  const Bounds = require( 'SIM_CORE/util/Bounds' );
+  const Util = require( 'SIM_CORE/util/Util' );
+  const Vector = require( 'SIM_CORE/util/Vector' );
+
+  class ModelViewTransform {
+
+    /**
+     * @param modelBounds {Bounds} the reference rectangle in the model, must have area > 0
+     * @param viewBounds {Bounds} the reference rectangle in the view, must have area > 0
+     */
+    constructor( modelBounds, viewBounds ) {
+
+      assert( modelBounds instanceof Bounds && modelBounds.area > 0, `invalid modelBounds: ${ modelBounds }` );
+      assert( viewBounds instanceof Bounds && viewBounds.area > 0, `invalid viewBounds: ${ viewBounds }` );
+
+      // @public final (read-only) {Bounds}
+      this.modelBounds = modelBounds;
+      this.viewBounds = viewBounds;
+
+      // @private {number} xViewToModelScale (in view units per model unit)
+      this.xViewToModelScale = viewBounds.width / modelBounds.width;
+
+      // @private {number} yViewToModelScale (in view units per model unit)
+      this.invertedYViewToModelScale = -viewBounds.height / modelBounds.height;
+
+      // @private {number} x-offset
+      this.xViewOffset = viewBounds.minX - this.xViewToModelScale * modelBounds.minX;
+      this.xModelOffset = modelBounds.minX - viewBounds.minX / this.xViewToModelScale;
+
+      // @private {number} yViewOffset
+      this.yViewOffset = viewBounds.minY - this.invertedYViewToModelScale * modelBounds.maxY;
+      this.yModelOffset = modelBounds.minY - viewBounds.minY / this.yViewToModelScale;
+    }
+  }
+} );
