94b8e38c4ae096f1522e9857eab8f127deb1211a
Brandon Li <brandon.li820@icloud.com>
2019-12-05 10:32:09 -0700

    fix type with model view transform #19

--- src/util/ModelViewTransform.js
+++ src/util/ModelViewTransform.js
@@ -22,6 +22,9 @@ define( require => {
   const Bounds = require( 'SIM_CORE/util/Bounds' );
   const Vector = require( 'SIM_CORE/util/Vector' );
 
+  // constants
+  const V = ( x, y ) => new Vector( x, y );
+
   class ModelViewTransform {
 
     /**
@@ -59,15 +62,15 @@ define( require => {
     // Coordinate Transforms
     modelToViewX( x ) { return this.xViewToModelScale * x + this.xViewOffset; }
     modelToViewY( y ) { return this.yViewToModelScale * y + this.yViewOffset; }
-    modelToViewXY( x, y ) { return new Vector( this.modelToViewX( x ), this.modelToViewY( y ) ); }
+    modelToViewXY( x, y ) { return V( this.modelToViewX( x ), this.modelToViewY( y ) ); }
 
     // Width/Height Transforms
     modelToViewDeltaX( x ) { return this.xViewToModelScale * x }
     modelToViewDeltaY( y ) { return this.yViewToModelScale * y }
-    modelToViewDelta( vector ) { return new Vector( this.modelToViewDeltaX( x ), this.modelToViewDeltaY( y ) ); }
+    modelToViewDelta( point ) { return V( this.modelToViewDeltaX( point.x ), this.modelToViewDeltaY( point.y ) ); }
 
     // Utilities
-    modelToViewPoint( point ) { return new Vector( this.modelToViewX( point.x ), this.modelToViewY( point.y ) ); }
+    modelToViewPoint( point ) { return V( this.modelToViewX( point.x ), this.modelToViewY( point.y ) ); }
     modelToViewBounds( bounds ) {
       return new Bounds(
         this.modelToViewX( bounds.minX ),
@@ -84,15 +87,15 @@ define( require => {
     // Coordinate Transforms
     viewToModelX( x ) { return x / this.xViewToModelScale + this.xModelOffset; }
     viewToModelY( y ) { return y / this.yViewToModelScale + this.yModelOffset; }
-    viewToModelXY( x, y ) { return new Vector( this.viewToModelX( x ), this.viewToModelY( y ) ); }
+    viewToModelXY( x, y ) { return V( this.viewToModelX( x ), this.viewToModelY( y ) ); }
 
     // Width/Height Transforms
     viewToModelDeltaX( x ) { return x / this.xViewToModelScale }
     viewToModelDeltaY( y ) { return y /this.yViewToModelScale }
-    viewToModelDelta( vector ) { return new Vector( this.modelToViewDeltaX( x ), this.modelToViewDeltaY( y ) ); }
+    viewToModelDelta( point ) { return V( this.modelToViewDeltaX( point.x ), this.modelToViewDeltaY( point.y ) ); }
 
     // Utilities
-    viewToModelPoint( point ) { return new Vector( this.viewToModelX( point.x ), this.viewToModelY( point.y ) ); }
+    viewToModelPoint( point ) { return V( this.viewToModelX( point.x ), this.viewToModelY( point.y ) ); }
     viewToModelBounds( bounds ) {
       return new Bounds(
         this.viewToModelX( bounds.minX ),
