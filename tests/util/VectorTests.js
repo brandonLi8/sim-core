02373774fa16a7d934cec6bbe469e5ffcb6f0859
Brandon Li <brandon.li820@icloud.com>
2020-01-28 17:18:33 -0700

    #25 use ABCD for vector names

--- tests/util/VectorTests.js
+++ tests/util/VectorTests.js
@@ -19,44 +19,44 @@ define( require => {
     //----------------------------------------------------------------------------------------
     // Define basic Vectors
     //----------------------------------------------------------------------------------------
-    const vector1 = new Vector( 4, 2 );
-    const vector2 = new Vector( 2, 3 );
-    const vector3 = new Vector( 5.2, 8.6 );
-    const vector4 = new Vector( 2, 3 );
+    const A = new Vector( 4, 2 );
+    const B = new Vector( 2, 3 );
+    const C = new Vector( 5.2, 8.6 );
+    const D = new Vector( 2, 3 );
 
     //----------------------------------------------------------------------------------------
     // Basic properties tests.
     //----------------------------------------------------------------------------------------
-    truenit.ok( vector1.x === 4 && vector1.y === 2, 'basic' );
-    truenit.ok( vector2.x === 2 && vector2.y === 3, 'basic' );
-    truenit.ok( vector3.x === 5.2 && vector3.y === 8.6, 'basic' );
-    truenit.ok( vector4.x === 2 && vector4.y === 3, 'basic' );
+    truenit.ok( A.x === 4 && A.y === 2, 'basic' );
+    truenit.ok( B.x === 2 && B.y === 3, 'basic' );
+    truenit.ok( C.x === 5.2 && C.y === 8.6, 'basic' );
+    truenit.ok( D.x === 2 && D.y === 3, 'basic' );
     truenit.ok( Vector.ZERO.x === 0 && Vector.ZERO.y === 0, 'basic' );
 
     //----------------------------------------------------------------------------------------
     // Distance and Magnitude tests
     //----------------------------------------------------------------------------------------
     const distance1And3 = ( 1.2 ** 2 + 6.6 ** 2 ) ** 0.5;
-    truenit.approximate( vector1.distanceTo( vector3 ), distance1And3 );
-    truenit.approximate( vector3.distanceTo( vector1 ), distance1And3 );
-    truenit.approximate( vector2.distanceTo( vector1 ), 5 ** 0.5 );
-    truenit.approximate( vector2.distanceTo( Vector.ZERO ), 13 ** 0.5 );
-    truenit.approximate( vector2.magnitude, 13 ** 0.5 );
-    truenit.approximate( vector2.getMagnitude(), 13 ** 0.5 );
+    truenit.approximate( A.distanceTo( C ), distance1And3 );
+    truenit.approximate( C.distanceTo( A ), distance1And3 );
+    truenit.approximate( B.distanceTo( A ), 5 ** 0.5 );
+    truenit.approximate( B.distanceTo( Vector.ZERO ), 13 ** 0.5 );
+    truenit.approximate( B.magnitude, 13 ** 0.5 );
+    truenit.approximate( B.getMagnitude(), 13 ** 0.5 );
     truenit.ok( Vector.ZERO.distanceTo( Vector.ZERO ) === 0 );
-    truenit.approximate( vector2.distanceTo( vector2 ), 0 );
+    truenit.approximate( B.distanceTo( B ), 0 );
 
     //----------------------------------------------------------------------------------------
     // Equality tests
     //----------------------------------------------------------------------------------------
-    const closeVector = new Vector( vector1.x + 0.0000001, vector1.y + 0.0000001 );
+    const closeVector = new Vector( A.x + 0.0000001, A.y + 0.0000001 );
 
-    truenit.ok( vector2.equals( vector4 ) );
-    truenit.ok( vector4.equals( vector2.copy() ) );
-    truenit.notOk( vector3.equals( vector2 ) );
-    truenit.notOk( vector1.equals( closeVector ) );
-    truenit.ok( vector1.equalsEpsilon( closeVector ) );
-    truenit.notOk( vector1.equals( closeVector ) );
+    truenit.ok( B.equals( D ) );
+    truenit.ok( D.equals( B.copy() ) );
+    truenit.notOk( C.equals( B ) );
+    truenit.notOk( A.equals( closeVector ) );
+    truenit.ok( A.equalsEpsilon( closeVector ) );
+    truenit.notOk( A.equals( closeVector ) );
 
     //----------------------------------------------------------------------------------------
     // IsFinite tests
@@ -64,7 +64,7 @@ define( require => {
     const nonFiniteVector1 = new Vector( 1.7976931348623157E+10308, 1.7976931348623157E+10308 );
     const nonFiniteVector2 = new Vector( Infinity, Infinity );
 
-    truenit.ok( vector1.isFinite() && vector2.isFinite() && vector3.isFinite() && Vector.ZERO.isFinite() );
+    truenit.ok( A.isFinite() && B.isFinite() && C.isFinite() && Vector.ZERO.isFinite() );
     truenit.notOk( nonFiniteVector1.isFinite() );
     truenit.notOk( nonFiniteVector1.isFinite() );
     truenit.notOk( nonFiniteVector2.isFinite() );
@@ -72,28 +72,28 @@ define( require => {
     //----------------------------------------------------------------------------------------
     // Test that the above tests didn't mutate anything
     //----------------------------------------------------------------------------------------
-    truenit.ok( vector1.x === 4 && vector1.y === 2 );
-    truenit.ok( vector2.x === 2 && vector2.y === 3 );
-    truenit.ok( vector3.x === 5.2 && vector3.y === 8.6 );
-    truenit.ok( vector4.x === 2 && vector4.y === 3 );
+    truenit.ok( A.x === 4 && A.y === 2 );
+    truenit.ok( B.x === 2 && B.y === 3 );
+    truenit.ok( C.x === 5.2 && C.y === 8.6 );
+    truenit.ok( D.x === 2 && D.y === 3 );
     truenit.ok( Vector.ZERO.x === 0 && Vector.ZERO.y === 0 );
 
     //----------------------------------------------------------------------------------------
     // To String
     //----------------------------------------------------------------------------------------
-    vector1.toString();
+    A.toString();
 
     //----------------------------------------------------------------------------------------
     // Mutators
     //----------------------------------------------------------------------------------------
 
     // Copy the vectors
-    const vector1Copy = vector1.copy();
-    const vector3Copy = vector3.copy();
-    const vector4Copy = vector4.copy();
+    const vector1Copy = A.copy();
+    const vector3Copy = C.copy();
+    const vector4Copy = D.copy();
 
-    vector1Copy.set( vector2 );
-    truenit.ok( vector1Copy.equals( vector2 ) );
+    vector1Copy.set( B );
+    truenit.ok( vector1Copy.equals( B ) );
 
     vector1Copy.setX( 0 );
     vector1Copy.setY( 0 );
@@ -104,18 +104,18 @@ define( require => {
     truenit.ok( vector3Copy.equals( vector4Copy ) );
 
     // normalize (which also tests divide and multiply)
-    truenit.ok( vector4.copy().normalize().equalsEpsilon(
-      new Vector( 2 / vector4.magnitude, 3 / vector4.magnitude )
+    truenit.ok( D.copy().normalize().equalsEpsilon(
+      new Vector( 2 / D.magnitude, 3 / D.magnitude )
     ) );
 
     // add and subtract
-    truenit.ok( vector1.copy().add( new Vector( 4, 4 ) ).equals( new Vector( 8, 6 ) ) );
-    truenit.ok( vector1.copy().subtract( new Vector( 4, 4 ) ).equals( new Vector( 0, -2 ) ) );
+    truenit.ok( A.copy().add( new Vector( 4, 4 ) ).equals( new Vector( 8, 6 ) ) );
+    truenit.ok( A.copy().subtract( new Vector( 4, 4 ) ).equals( new Vector( 0, -2 ) ) );
 
-    truenit.ok( vector1.x === 4 && vector1.y === 2 );
-    truenit.ok( vector2.x === 2 && vector2.y === 3 );
-    truenit.ok( vector3.x === 5.2 && vector3.y === 8.6 );
-    truenit.ok( vector4.x === 2 && vector4.y === 3 );
+    truenit.ok( A.x === 4 && A.y === 2 );
+    truenit.ok( B.x === 2 && B.y === 3 );
+    truenit.ok( C.x === 5.2 && C.y === 8.6 );
+    truenit.ok( D.x === 2 && D.y === 3 );
     truenit.ok( Vector.ZERO.x === 0 && Vector.ZERO.y === 0 );
 
   };
