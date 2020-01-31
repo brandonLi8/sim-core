fe839130388879fda3189c049eeb205dc59aafac
Brandon Li <brandon.li820@icloud.com>
2020-01-31 12:50:09 -0700

    #31 clean up Multilink

--- src/util/Multilink.js
+++ src/util/Multilink.js
@@ -1,7 +1,8 @@
 // Copyright © 2019-2020 Brandon Li. All rights reserved.
 
 /**
- * A Multilink is a utility class for linking to multiple Properties with the same callback functionality.
+ * A Multilink is a utility class for linking to multiple Properties with the same callback functionality. It is
+ * recommended to read the documentation in `SIM_CORE/util/Property` before working with this class.
  *
  * A Multilink is constructed with an array of Properties called the dependencies, and a callback function.
  * When any of the Properties in the dependencies change, the callback function is called, with the values of the
@@ -10,7 +11,7 @@
  * A Multilink is not a sub-type of Property and does not contain a value. However, it does contain internal links
  * to the dependencies. If the Multilink is no longer in use, make sure to call the dispose() method to unlink
  * the internal listener to avoid a memory leak.
-
+ *
  * @author Brandon Li <brandon.li820@gmail.com>
  */
 
@@ -19,58 +20,79 @@ define( require => {
 
   // modules
   const assert = require( 'SIM_CORE/util/assert' );
+  const Property = require( 'SIM_CORE/util/Property' );
   const Util = require( 'SIM_CORE/util/Util' );
 
   class Multilink {
 
     /**
-     * @param {Property[]} dependencies - array of Properties to listen to
-     * @param {function} callback - function that expects args in the same order as dependencies
-     * @param {boolean} [lazy] Optional parameter that can be set to true if this should be a lazy multilink.
+     * @param {Property[]} dependencies - array of Properties to link to.
+     * @param {function} callback - function called when any of the Properties in dependencies change. The values of the
+     *                              dependencies are passed (in the same order) to this function.
+     * @param {boolean} [lazy=false] - Optional parameter that can be set to true if this should be a lazy Multilink.
      */
-    constructor( dependencies, callback, lazy ) {
-
-      assert( Util.isArray( dependencies ), `invalid dependencies: ${ dependencies }` );
+    constructor( dependencies, callback, lazy = false ) {
+      assert( Util.isArray( dependencies ) && dependencies.every( dependency => dependency instanceof Property ),
+        `invalid dependencies: ${ dependencies }` );
       assert( typeof callback === 'function', `invalid callback: ${ callback }` );
+      assert( typeof lazy === 'boolean', `invalid lazy: ${ lazy }` );
 
       //----------------------------------------------------------------------------------------
 
-      // @private {Property[]}
+      // @private {Property[]} - reference to the dependencies array.
       this._dependencies = dependencies;
 
-      // @private - whether the Multilink has been disposed
-      this.isDisposed = false;
+      // @private {boolean} - indicates whether the Multilink has been disposed. See dispose() for more documentation.
+      this._isDisposed = false;
 
-      // @private listener linked to all properties
+      // @private {function} - listener linked to all the dependencies that calls the callback function. The values of
+      //                       the dependencies are passed to the callback function in the same order.
       this._listener = () => {
-        if ( !this.isDisposed ) {
-          callback( ...this._dependencies.map( property => property.value ) );
-        }
+        callback( ...this._dependencies.map( property => property.value ) );
       };
 
-      // When a dependency value changes, update the list of dependencies and call back to the callback
+      // Lazily link the listener to each dependency for now. When any of the dependencies change, the
+      // callback should be called.
       dependencies.forEach( dependency => {
         dependency.lazyLink( this._listener );
       } );
 
-      // Initial call back, only once
+      // If the Multilink isn't lazy, call back the callback function for the initial call back.
       if ( !lazy ) this._listener();
     }
 
-    // @public
+    /**
+     * Disposes the Multilink and ensures that it can be garbage collected.
+     * @public
+     *
+     * @returns {Multilink} - for chaining.
+     */
     dispose() {
-      assert( this.isDisposed === false, 'A Multilink cannot be disposed twice.' );
+      assert( this._isDisposed === false, 'A Multilink cannot be disposed twice.' );
 
+      // Unlink the internal listener from each Property in the dependencies array.
       this._dependencies.forEach( dependency => {
         dependency.unlink( this._listener );
       } );
+
+      // Reset references
       this._dependencies = null;
       this._listener = null;
-      this.isDisposed = true;
+
+      // Indicate that this Multilink has been disposed.
+      this._isDisposed = true;
     }
   }
 
-
+  /**
+   * Returns a new Multilink object that is lazily constructed.
+   * @public
+   *
+   * @param {Property[]} dependencies - array of Properties to link to.
+   * @param {function} callback - function called when any of the Properties in dependencies change. The values of the
+   *                              dependencies are passed (in the same order) to this function.
+   * @returns {Multilink}
+   */
   Multilink.lazy = ( dependencies, callback ) => new Multilink( dependencies, callback, true );
 
   return Multilink;
