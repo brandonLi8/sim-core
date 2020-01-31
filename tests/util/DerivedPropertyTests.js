56b6cbe24a9589b431631f49d4ac79e7365e1034
Brandon Li <brandon.li820@icloud.com>
2020-01-30 21:33:14 -0700

    #30 Define basic tests

--- tests/util/DerivedPropertyTests.js
+++ tests/util/DerivedPropertyTests.js
@@ -13,10 +13,58 @@ define( require => {
   const DerivedProperty = require( 'SIM_CORE/util/DerivedProperty' );
   const Property = require( 'SIM_CORE/util/Property' );
   const truenit = require( 'truenit' );
-  const Vector = require( 'SIM_CORE/util/Vector' );
 
   return () => {
 
+    // Define basic Properties. These will change in the tests.
+    const A = new Property( 1 );
+    const B = new Property( 2 );
+    const C = new Property( 3 );
+    const D = new Property( 4 );
+
+    // Create the DerivedProperty based on A, B, C, D
+    const SumProperty = new DerivedProperty(
+      [ A, B, C, D ],
+      ( a, b, c, d  ) => {
+
+        // Check that the passed in values correlate to A, B, C, D
+        truenit.ok( a === A.value && b === B.value && c === C.value && d === D.value );
+
+        // Return the sum of A, B, C, D
+        return a + b + c + d;
+      } );
+
+    //----------------------------------------------------------------------------------------
+
+    // Test the initial value of SumProperty
+    truenit.ok( SumProperty.value === 1 + 2 + 3 + 4 );
+
+    // Mutators and initialValue should throw errors
+    truenit.throws( () => { SumProperty.set( 5 ); } );
+    truenit.throws( () => { SumProperty.value = 5; } );
+    truenit.throws( () => { SumProperty.reset(); } );
+    truenit.throws( () => { SumProperty.getInitialValue(); } );
+    truenit.throws( () => { SumProperty.initialValue; } );
+
+    // Mutate each Property and check that the value of SumProperty is correct.
+    A.set( 0 );
+    truenit.ok( SumProperty.value === 0 + 2 + 3 + 4 );
+
+    B.set( 0 );
+    truenit.ok( SumProperty.value === 0 + 0 + 3 + 4 );
+
+    C.set( 0 );
+    truenit.ok( SumProperty.value === 0 + 0 + 0 + 4 );
+
+    D.set( 0 );
+    truenit.ok( SumProperty.value === 0 + 0 + 0 + 0 );
+
+    // Reset each Property and check that the value of SumProperty is correct.
+    A.reset();
+    B.reset();
+    C.reset();
+    D.reset();
+    truenit.ok( SumProperty.value === 1 + 2 + 3 + 4 );
 
   };
 } );
