624d7eeeb3abce80daf50fd9022a6f9b4ef006f0
Brandon Li <brandon.li820@icloud.com>
2020-01-30 21:12:02 -0700

    #30 clean up derivedProperty

--- src/util/DerivedProperty.js
+++ src/util/DerivedProperty.js
@@ -7,7 +7,8 @@
  * derivation function.
  *
  * A DerivedProperty cannot be directly mutated. Setters should not be called directly, and the mutators will throw an
- * error if used directly.
+ * error if used directly. If the DerivedProperty is no longer in use, make sure to call the dispose() method to unlink
+ * the internal DerivedProperty listeners to avoid a memory leak.
  *
  * @author Brandon Li <brandon.li820@gmail.com>
  */
@@ -23,67 +24,81 @@ define( require => {
   class DerivedProperty extends Property {
 
     /**
-     * @param {Property[]} dependencies - Properties that this Property's value is derived from
-     * @param {function} derivation - function that derives this Property's value,
-     *                                expects args in the same order as dependencies
-     * @param {Object} [options] - see Property
+     * @param {Property[]} dependencies - Properties that this Property's value is derived from.
+     * @param {function} derivation - function that derives this Property's value. The values of the dependencies will
+     *                                be passed (in the same order) to this function. This function should return
+     *                                the newly derived value of the Property.
+     * @param {Object} [options] - passed to the super class.
      */
     constructor( dependencies, derivation, options ) {
-
-      assert( Util.isArray( dependencies ), `invalid dependencies: ${ dependencies }` );
+      assert( Util.isArray( dependencies ) && dependencies.every( dependency => dependency instanceof Property ),
+        `invalid dependencies: ${ dependencies }` );
       assert( typeof derivation === 'function', `invalid derivation: ${ derivation }` );
 
+      // Derive the initial value of the Property based on what the derivation initially returns. The values of the
+      // dependencies are passed to the derivation in the same order.
       const initialValue = derivation( ...dependencies.map( property => property.value ) );
+
       super( initialValue, options );
 
       //----------------------------------------------------------------------------------------
 
-      // @private {Property[]}
+      // @private {Property[]} - reference to the dependencies array.
       this._dependencies = dependencies;
 
-      this._isSettingInternally = false;
-
-      // @private listener linked to all properties
+      // @private {function} - listener linked to all the dependencies that sets the value of the this Property based on
+      //                       what the derivation fucntion returns. The values of the dependencies are passed to the
+      //                       derivation in the same order.
       this._listener = () => {
+
+        // Use super.set since this.set throws and error. See set() or the comment at the top of the file for more doc.
         super.set( derivation( ...dependencies.map( property => property.value ) ) );
       };
 
-      // DerivedProperty cannot be set, so we don't store the initial value to help prevent memory issues.
+      // DerivedProperty cannot be mutated, so we don't store the initial value to help prevent memory issues.
       this._initialValue = null;
 
-      // When a dependency value changes, update the list of dependencies and call back to the callback
+      // Lazily link the listener to each dependency since the value is already set to the initial derivation.
+      // When any of the dependencies change, the derivation should be called and the value of the DerivedProperty
+      // should be set.
       dependencies.forEach( dependency => {
         dependency.lazyLink( this._listener );
       } );
     }
 
-    // @public
+    /**
+     * Disposes the DerivedProperty and ensures that it can be garbage collected.
+     * @override
+     * @public
+     *
+     * @returns {Property} - for chaining.
+     */
     dispose() {
 
-      // Unlink from dependent Properties
+      // Unlink the listener from each dependency.
       this._dependencies.forEach( dependency => {
         dependency.unlink( this._listener );
       } );
+
       this._dependencies = null;
       this._listener = null;
 
-      super.dispose();
+      return super.dispose();
     }
 
     /**
-     * Override the mutators to provide an error message.  These should not be called directly,
-     * the value should only be modified when the dependencies change.
-     * @param value
+     * DerivedProperties cannot be mutated. Overridden to throw an error if called.
      * @override
      * @public
+     *
+     * @param {*} value
      */
     set( value ) { assert( false, 'Cannot set values directly to a DerivedProperty, tried to set: ' + value ); }
 
     /**
-     * Prevent the retrieval of the initial value, since we don't store it.
-     * @public
+     * Prevent the retrieval of the initial value, since it isn't stored.
      * @override
-     * @returns {*}
+     * @public
      */
     getInitialValue() { assert( false, 'Cannot get the initial value of a DerivedProperty' ); }
   }
