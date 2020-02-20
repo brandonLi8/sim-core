be4ea349bfc95d880971321e7e038f6be396bf31
Brandon Li <brandon.li820@icloud.com>
2020-02-20 13:00:51 -0700

    Add skeleton of Shape.js #35

---
+++ src/util/Shape.js
@@ -0,0 +1,20 @@
+// Copyright © 2020 Brandon Li. All rights reserved.
+
+/**
+ *
+ * @author Brandon Li <brandon.li820@gmail.com>
+ */
+
+define( require => {
+  'use strict';
+
+  // modules
+  const assert = require( 'SIM_CORE/util/assert' );
+  const Util = require( 'SIM_CORE/util/Util' );
+  const Vector = require( 'SIM_CORE/util/Vector' );
+
+  class Shape {
+  }
+
+  return Shape;
+} );
