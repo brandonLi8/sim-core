a0fa63cfaa4db428fe9f19f66c14a609d2b5a3d7
Brandon Li <brandon.li820@icloud.com>
2020-03-12 13:39:35 -0600

    Add circle to scenery

--- src/scenery/Circle.js
+++ src/scenery/Circle.js
@@ -1,8 +1,9 @@
 // Copyright © 2019-2020 Brandon Li. All rights reserved.
 
-// NOTE: THIS DOESNT WORK ATM!!!
 /**
- * A sim-specific Circle node for SVG (scalable vector graphics).
+ * A Circle Node that displays a complete circular shape, with different radii.
+ *
+ * Circle inherits from Path to allow for different strokes, fills, shapeRenderings, etc.
  *
  * @author Brandon Li <brandon.li820@gmail.com>
  */
@@ -12,61 +13,80 @@ define( require => {
 
   // modules
   const assert = require( 'SIM_CORE/util/assert' );
-  const Node = require( 'SIM_CORE/scenery/Node' );
+  const Path = require( 'SIM_CORE/scenery/Path' );
+  const Shape = require( 'SIM_CORE/util/Shape' );
+  const Util = require( 'SIM_CORE/util/Util' );
 
-  // constants
-
-  class Circle extends Node {
+  class Circle extends Path {
 
     /**
-     * @param {Object} [options] - Various key-value pairs that control the appearance and behavior. Subclasses
-     *                             may have different options for their API. See the code where the options are set in
-     *                             the early portion of the constructor for details.
+     * @param {number} radius - the radius (non-negative) of the circle
+     * @param {Object} [options] - Various key-value pairs that control the appearance and behavior. All options are
+     *                             passed to the super class.
      */
-    constructor( options ) {
+    constructor( radius, options ) {
+      assert( typeof radius === 'number' && radius >= 0, `invalid radius: ${ radius }` );
       assert( !options || Object.getPrototypeOf( options ) === Object.prototype, `invalid options: ${ options }` );
+      assert( !options || !options.attributes, 'Circle sets attributes' );
 
-      // Defaults for options.
-      const defaults = {
+      // There are no options specific to Circle. Pass options to super class.
+      super( null, options );
 
-        type: 'circle',
-        radius: 0
-      };
-
-      // Rewrite options so that it overrides the defaults.
-      options = { ...defaults, ...options };
-      super( options );
-
-      this._radius = options.radius;
-      this.radius = this._radius;
-
-    }
-
-    get radius() { return this._radius; }
-    set radius( radius ) {
+      // @private {number} - reference to the radius of the Circle.
       this._radius = radius;
-
-      const center = this.center.copy();
-      this.width = this._radius * 2;
-
-      this.height = this._radius * 2;
-
-      this.center = center;
-      this.layout( this._screenViewScale );
+      this.updateCircleShape();
     }
 
-    layout( scale ) {
+    /**
+     * ES5 getters of the radius of the Circle. Traditional Accessors methods isn't included to reduce the memory
+     * footprint.
+     * @public
+     *
+     * @returns {number}
+     */
+    get radius() { return this._radius; }
 
-      super.layout( scale );
-      const globalBounds = this._computeGlobalBounds();
+    //----------------------------------------------------------------------------------------
 
-      globalBounds && this.addAttributes( {
-        r: `${ scale * this._radius }px`,
-        cx: `${ this._radius * scale }`,
-        cy: `${ this._radius * scale }`
-      } );
+    /**
+     * Ensures that `set shape` is not called on a Circle.
+     * @public
+     *
+     * @param {Shape} shape
+     */
+    set shape( shape ) { assert( false, 'Circle sets shape' ); }
+
+    /**
+     * Sets the radius of the Circle.
+     * @public
+     *
+     * @param {number} radius
+     */
+    set radius( radius ) {
+      if ( radius === this._radius ) return; // Exit if setting to the same 'radius'
+      assert( typeof radius === 'number', `invalid radius: ${ radius }` );
+      this._radius = radius;
+      this.updateCircleShape();
+    }
+
+    /**
+     * Generates a Circle shape and updates the shape of this Circle. Called when a property or the Circle that is
+     * displayed is changed, resulting in a different Circle Shape.
+     * @private
+     */
+    updateCircleShape() {
+      // Must be a valid Circle Node.
+      if ( this._radius ) {
+        if ( this._radius === 0 ) super.shape = null; // Set to null shape if the radius is 0.
+
+        // Generate the Circle Shape.
+        super.shape = new Shape().arc( this.center, this._radius, 0, 2 * Math.PI - Util.EPSILON ).close();
+      }
     }
   }
+
+  // @protected @override {string[]} - setter names specific to Circle. See Path.MUTATOR_KEYS for documentation.
+  Circle.MUTATOR_KEYS = Path.MUTATOR_KEYS;
 
   return Circle;
 } );
