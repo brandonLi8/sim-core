06fabe167da4a82936b009fa47b698f57708577a
Brandon Li <brandon.li820@icloud.com>
2020-03-17 13:22:16 -0600

    #39 doc in Gradient

---
+++ src/scenery/gradients/Gradient.js
@@ -0,0 +1,12 @@
+// Copyright © 2020 Brandon Li. All rights reserved.
+
+/**
+ * Abstract class for Gradients, specifically LinearGradient and RadialGradient. Not meant to be instantiated directly.
+ *
+ * A Gradient refers to smooth transition of one color to another color within a shape.
+ * Gradients can be applied as 'fills' and 'strokes' in Path and Text.
+ *
+ * For general context on gradients in SVG, see https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorial/Gradients.
+ *
+ * @author Brandon Li <brandon.li820@gmail.com>
+ */
