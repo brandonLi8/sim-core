3a4ddc66cf9afa70566a01cc64fdde1d16368dbe
Brandon Li <brandon.li820@icloud.com>
2020-03-19 15:41:55 -0600

    #40 add color wheel class

---
+++ src/util/ColorWheel.js
@@ -0,0 +1,20 @@
+// Copyright © 2020 Brandon Li. All rights reserved.
+
+/**
+ * Color Wheel Doc
+ *
+ * @author Brandon Li <brandon.li820@gmail.com>
+ */
+
+define( require => {
+  'use strict';
+
+  // modules
+  const assert = require( 'SIM_CORE/util/assert' );
+  const Util = require( 'SIM_CORE/util/Util' );
+
+  class ColorWheel {
+  }
+
+  return ColorWheel;
+} );
