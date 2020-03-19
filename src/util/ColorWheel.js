1b3d5a4fcebc063add3f098a92a877d718a4cac2
Brandon Li <brandon.li820@icloud.com>
2020-03-19 16:23:28 -0600

    #40 Add doc for color wheel

--- src/util/ColorWheel.js
+++ src/util/ColorWheel.js
@@ -1,7 +1,21 @@
 // Copyright © 2020 Brandon Li. All rights reserved.
 
 /**
- * Color Wheel Doc
+ * Color Wheel is a utility class for shading, highlighting, and converting colors in a variety of formats.
+ * Before using ColorWheel, it is important to be familiar with CSS color strings. See
+ * https://developer.mozilla.org/en-US/docs/Web/CSS/color_value for the official specification.
+ *
+ * Color Wheel is designed to work with most CSS color strings, including:
+ *   - rgb        e.g. 'rgb(104, 200, 82)'
+ *   - rgba       e.g. 'rgb(104, 200, 82, 100)'
+ *   - hex        e.g. '#1BC4FD'
+ *   - hex-alpha  e.g. '#1BC4FDF2'
+ *   - hsl        e.g. 'hsl(270 60% 70%)`
+ *   - hsla       e.g. 'hsla(240, 100%, 50%, .7)'
+ *   - keywords   e.g. 'aqua', 'transparent', etc.
+ *
+ * NOTE: Currently, Color Wheel doesn't support space-separated values in functional notation. Additionally,
+ *       the first parameter of HSL and HSLA (hue) must be unit-less (in degrees).
  *
  * @author Brandon Li <brandon.li820@gmail.com>
  */
@@ -11,6 +25,7 @@ define( require => {
 
   // modules
   const assert = require( 'SIM_CORE/util/assert' );
+  const DOMObject = require( 'SIM_CORE/core-internal/DOMObject' );
   const Util = require( 'SIM_CORE/util/Util' );
 
   class ColorWheel {
