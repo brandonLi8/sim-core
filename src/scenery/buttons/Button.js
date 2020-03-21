35d180c4e8b8d2b811d9b9ce66ddb9bd58d338cf
Brandon Li <brandon.li820@icloud.com>
2020-03-20 22:26:25 -0600

    #42 Add basic structure for button

--- src/scenery/buttons/Button.js
+++ src/scenery/buttons/Button.js
@@ -8,9 +8,38 @@
  *   - Handling Drag/Hover/Press Listeners for handling user interaction with the Button.
  *   - Positioning content on-top of a background Path sub-type.
  *   - Providing a static method that makes a Button's background look 3D by highlighting and shading different portions
- *     of a Gradient based off a base-color and returning the Gradient (to be applied as the 'fill' of a Path).
+ *     of a Gradient based off a base-color and applying it as the 'fill'. Will also listen to when the interaction
+ *     state of the Button changes to modify the gradient slightly when the button is hovered.
  *
  * @author Brandon Li <brandon.li820@gmail.com>
  */
 
+define( require => {
+  'use strict';
 
+  // modules
+  const assert = require( 'SIM_CORE/util/assert' );
+  const HoverListener = require( 'SIM_CORE/scenery/events/HoverListener' );
+  const Node = require( 'SIM_CORE/scenery/Node' );
+  const Path = require( 'SIM_CORE/scenery/Path' );
+  const PressListener = require( 'SIM_CORE/scenery/events/PressListener' );
+
+  class Button extends Node {
+
+    /**
+     * @param {Path} background - the background Path (usually sub-type) of the Button
+     * @param {Node} content - the content of the Button, placed on top of the background
+     * @param {Object} [options] - Various key-value pairs that control the appearance and behavior. Subclasses
+     *                             may have different options for their API. See the code where the options are set in
+     *                             the early portion of the constructor for details.
+     */
+    constructor( background, content, options ) {
+      assert( background instanceof Path, `invalid background: ${ background }` );
+      assert( content instanceof Node, `invalid content: ${ content }` );
+      assert( !options || Object.getPrototypeOf( options ) === Object.prototype, `invalid options: ${ options }` );
+
+    }
+  }
+
+  return Button;
+} );
