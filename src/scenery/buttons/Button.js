67e01d74429b62d45b91032bda312513b4da8cd6
Brandon Li <brandon.li820@icloud.com>
2020-03-20 21:40:47 -0600

    #42 add doc to button

--- src/scenery/buttons/Button.js
+++ src/scenery/buttons/Button.js
@@ -1,2 +1,16 @@
 // Copyright © 2019-2020 Brandon Li. All rights reserved.
-// NOTE: THIS DOESNT WORK ATM!!!
+
+/**
+ * A base-class abstraction for all Button Node sub-types. This class is generally not meant to be used within
+ * sim-specific code. Rather, use any of its subclasses.
+ *
+ * The Button class is responsible for:
+ *   - Handling Drag/Hover/Press Listeners for handling user interaction with the Button.
+ *   - Positioning content on-top of a background Path sub-type.
+ *   - Providing a static method that makes a Button's background look 3D by highlighting and shading different portions
+ *     of a Gradient based off a base-color and returning the Gradient (to be applied as the 'fill' of a Path).
+ *
+ * @author Brandon Li <brandon.li820@gmail.com>
+ */
+
+
