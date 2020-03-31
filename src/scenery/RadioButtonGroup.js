dc1cb1750f3acfa758a2204365fc2530ef26f288
Brandon Li <brandon.li820@icloud.com>
2020-03-30 21:28:02 -0600

    first attempt at RadioButtonGroup

--- src/scenery/RadioButtonGroup.js
+++ src/scenery/RadioButtonGroup.js
@@ -25,18 +25,67 @@ define( require => {
   const FlexBox = require( 'SIM_CORE/scenery/FlexBox' );
   const Property = require( 'SIM_CORE/util/Property' );
   const RadioButton = require( 'SIM_CORE/scenery/buttons/RadioButton' );
+  const Util = require( 'SIM_CORE/util/Util' );
+  const Button = require( 'SIM_CORE/scenery/buttons/Button' );
 
   class RadioButtonGroup extends FlexBox {
 
     /**
+     * @param {string} orientation - orientation: either 'horizontal' (a row) or 'vertical' (a column)
      * @param {Property.<Enum.Member>} enumProperty - the enumeration Property that is toggled when RadioButtons
      *                                                are selected.
      * @param {RadioButton[]} radioButtons - the RadioButtons of this RadioButtonGroup.
      * @param {Object} [options] - Various key-value pairs that control the appearance and behavior of the
      *                             RadioButtonGroup. All options are passed to the super class.
      */
-    constructor( enumProperty, radioButtons, options ) {
+    constructor( orientation, enumProperty, radioButtons, options ) {
+      assert( orientation === 'horizontal' || orientation === 'vertical', `invalid orientation: ${ orientation }` );
+      assert( enumProperty instanceof Property && enumProperty.value instanceof Enum.Member,
+        `invalid enumProperty: ${ enumProperty }` );
+      assert( Util.isArray( radioButtons ) && radioButtons.every( radioButton => radioButton instanceof RadioButton ),
+        `invalid radioButtons: ${ radioButtons }` );
+      assert( !options || Object.getPrototypeOf( options ) === Object.prototype, `invalid options: ${ options }` );
+      assert( !options || !options.children, 'RadioButtonGroup sets children' );
 
+      super( orientation, { ...options, children: radioButtons } );
+
+      //----------------------------------------------------------------------------------------
+
+      // @private {Object} - object literal that maps Radio Buttons to listeners of its interactionStateProperty.
+      //                     Used in the dispose method to remove listeners.
+      this._radioButtonListenerMap = {};
+
+      // Loop through each RadioButton in the passed-in radio Buttons array.
+      radioButtons.forEach( radioButton => {
+
+        // Check that we haven't already linked to this RadioButton.
+        assert( !this._radioButtonListenerMap[ radioButton ], `duplicate radio button found: ${ radioButton }` );
+
+        // Map a listener to the interactionStateProperty of the RadioButton.
+        this._radioButtonListenerMap[ radioButton ] = interactionState => {
+
+          // If the current radioButton is pressed, unselect all other RadioButtons and select the current radioButton.
+          if ( interactionState === Button.interactionStates.PRESSED ) {
+            radioButtons.forEach( radioButton => { radioButton.unselect(); } );
+            radioButton.select();
+          }
+        };
+
+        // Link the listener, which will be unlinked in the dispose method.
+        radioButton.interactionStateProperty.link( this._radioButtonListenerMap[ radioButton ] );
+      } );
+    }
+
+    /**
+     * @override
+     * Disposes the RadioButtonGroup and its internal listeners.
+     * @public
+     */
+    dispose() {
+      Object.keys( this._radioButtonListenerMap ).forEach( radioButton => {
+        radioButton.interactionStateProperty.unlink( this._radioButtonListenerMap[ radioButton ] );
+      } );
+      super.dispose();
     }
   }
 
